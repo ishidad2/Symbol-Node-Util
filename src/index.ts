8a83f3c1532bd2b39f6d0224305861e595c35303
ishidad2 <d.m.y.s.happy.life@gmail.com>
2022-07-28 17:35:32 +0900

    first commit

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+const sym = require('symbol-sdk');
+const config = require('./config/nodeConfig.js');
+import axios, { AxiosInstance } from 'axios';
+
+export const getActiveNode = async (networkType?: number) => {
+  const network = networkType ?? sym.NetworkType.TEST_NET;
+  const node = await getNode(network);
+  return node;
+};
+
+const client: AxiosInstance = axios.create({
+  timeout: 800, // milliseconds
+});
+
+const getNode = async (network: number): Promise<string> => {
+  let nodes = config.TEST_NET_NODES;
+  if (network === sym.NetworkType.MAIN_NET) {
+    nodes = config.MAIN_NET_NODES;
+  }
+  const node = nodes[Math.floor(Math.random() * nodes.length)];
+  return client
+    .get(node + '/node/health')
+    .then((res) => {
+      if (res.data.status.apiNode === 'up' && res.data.status.db === 'up') {
+        return node;
+      } else {
+        return getNode(network);
+      }
+    })
+    .catch((err) => {
+      return getNode(network);
+    });
+};
