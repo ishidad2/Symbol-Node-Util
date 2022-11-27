35c72899ace552582302ace7893d31a799283eb3
ishidad2 <ishida_d@opening-line.co.jp>
2022-11-27 16:47:43 +0900

    ヘルスチェック方法変更　generationHashを比較するように修正

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,82 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const config = require('./config/nodeConfig.js');
-import axios, { AxiosInstance } from 'axios';
-const MAIN_NET: number = 104;
-const TEST_NET: number = 152;
+import { RepositoryFactoryHttp, NetworkType } from "symbol-sdk"
+import { firstValueFrom } from 'rxjs'
 
-export const getActiveNode = async (networkType?: number | undefined) => {
-  let network = networkType;
-  if (networkType === undefined) {
-    network = TEST_NET;
+interface NodeInfo {
+  host: string,
+  apiNode: string,
+  db: string,
+  generationHash: string,
+  isHealth: boolean,
+}
+
+function isHealth(host: string,
+  apiNode: string,
+  db: string,
+  refNodeInfo?: NodeInfo) {
+  if (!refNodeInfo) {
+      return true;
   }
-  const node = await getNode(network);
-  return node;
-};
+  if (refNodeInfo) {
+      if (apiNode === 'up' && db === 'up') {
+        return true;
+      }
+  }
+  return false;
+}
 
-const client: AxiosInstance = axios.create({
-  timeout: 800, // milliseconds
-});
+async function getNodeInfo(nodeUrl: string) {
+  try {
+      const repoFactory = new RepositoryFactoryHttp(nodeUrl);
+      const nodeRepo = repoFactory.createNodeRepository();
+      const nodeHealth = await firstValueFrom(nodeRepo.getNodeHealth());
+      const nodeInfo = await firstValueFrom(nodeRepo.getNodeInfo());
 
-const getNode = async (network: number | undefined): Promise<string> => {
+      const _nodeInfo: NodeInfo = {
+          host: nodeUrl,
+          apiNode: nodeHealth.apiNode,
+          db: nodeHealth.db,
+          generationHash: nodeInfo.networkGenerationHashSeed,
+          isHealth: isHealth(nodeUrl, nodeHealth.apiNode, nodeHealth.db),
+
+      };
+      return _nodeInfo;
+  } catch (err) {
+      const _nodeInfo: NodeInfo = {
+          host: nodeUrl,
+          generationHash: '',
+          apiNode: 'down',
+          db: 'down',
+          isHealth: false,
+      };
+      return _nodeInfo;
+  }
+}
+
+async function NodeInfo(network: NetworkType): Promise<string> {
   let nodes = config.TEST_NET_NODES;
-  if (network === MAIN_NET) {
+  if (network === NetworkType.MAIN_NET) {
     nodes = config.MAIN_NET_NODES;
   }
   const node = nodes[Math.floor(Math.random() * nodes.length)];
-  return client
-    .get(node + '/node/health')
-    .then((res) => {
-      if (res.data.status.apiNode === 'up' && res.data.status.db === 'up') {
-        return node;
-      } else {
-        return getNode(network);
+  const nodeInfo = await getNodeInfo(node);
+  if(nodeInfo.apiNode === "up" && nodeInfo.db === "up"){
+    if(network === NetworkType.TEST_NET){
+      if(nodeInfo.generationHash === config.TEST_NET_GENERATIONHASH){
+        return nodeInfo.host;
       }
-    })
-    .catch((err) => {
-      return getNode(network);
-    });
+      return NodeInfo(network);
+    }
+    return nodeInfo.host;
+  }
+  return NodeInfo(network);
+}
+
+export const getActiveNode = async (networkType?: NetworkType) => {
+  let network = networkType;
+  if (!network) {
+    network = NetworkType.TEST_NET;
+  }
+  return await NodeInfo(network)
 };
